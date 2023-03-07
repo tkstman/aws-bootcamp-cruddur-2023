0029c33b1ff7240f28f46e01e278b8b162546db9
tkstman <tks.tman@gmail.com>
2023-03-07 02:08:48 +0000

    rollbar test week2

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -18,7 +18,6 @@ from services.show_activity import *
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-
 #These are the honeycomb.io telemetry tools
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
@@ -32,6 +31,11 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor, ConsoleSpanExpor
 import watchtower
 import logging
 from time import strftime
+
+#rollbar ---
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 
 """ LOGGER = logging.getLogger(__name__)
@@ -81,6 +85,33 @@ def after_request(response):
 """ xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 XRayMiddleware(app, xray_recorder) """
+
+
+
+#rollbar ---
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+    
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
